2c80ebe58b1bb712b720ab6756e763db760a0edb
JManuelPorras <josemanuelporrasguerra@gmail.com>
2024-12-13 13:48:12 -0500

    Fix repo

---
+++ Ficha.cs
@@ -0,0 +1,28 @@
+namespace TeVasAMorir
+{
+    class Ficha
+    {
+        public string Tipo {get; set;}
+        public int xPosicion {get; set;}
+        public int yPosicion {get; set;}
+
+        public Ficha(string tipo, int xposicion, int yposicion)
+        {
+            Tipo = tipo;
+            xPosicion = xposicion;
+            yPosicion = yposicion;
+        }
+
+        public void MoverFicha(int xnuevo, int ynuevo)
+        {
+            xPosicion = xnuevo;
+            yPosicion = ynuevo;
+        }
+
+        public override string ToString()
+        {
+            return $"Ficha: {Tipo} en ({xPosicion}, {yPosicion})";
+        }
+
+    }
+}
---
+++ Jugador.cs
@@ -0,0 +1,31 @@
+namespace TeVasAMorir
+{
+    class Jugador
+    {
+        public string Nombre {get; set;}
+        public int Puntuacion {get; set;}
+        public List<Ficha> Fichas {get; set;}
+
+        public Jugador(string nombre)
+        {
+            Nombre = nombre;
+            Puntuacion = 0;
+            Fichas = new List<Ficha>();
+        }
+
+        public void CogerFichas(Ficha ficha)
+        {
+            Fichas.Add(ficha);
+        }
+
+        public void IncrementarPuntuacion(int puntos)
+        {
+            Puntuacion += puntos;
+        }
+
+        public override string ToString()
+        {
+            return $"Jugador: {Nombre} ,Puntuacion: {Puntuacion} ,Fichas: {Fichas.Count}";
+        }
+    }
+}
